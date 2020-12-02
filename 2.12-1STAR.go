74fe26ea5202a11998d33d3873ab47a4b9f2742b
deshimonogit <52777375+deshimonogit@users.noreply.github.com>
2020-12-02 20:34:39 +0100

    Add files via upload

--- 2.12-1STAR.go
+++ 2.12-1STAR.go
@@ -38,16 +38,24 @@ func checkPasswords(lines []string){
 	
 	for i := 0; i<len(lines); i++{
 		if i == 0 || i%3 == 0{
+			controlSum := 0
 			pwRange := strings.Split(lines[i], "-")
 			pwMin, _ := strconv.ParseInt(pwRange[0], 10, 64)
 			pwMax, _ := strconv.ParseInt(pwRange[1], 10, 64)
 			pwLetter := string(lines[i+1][0])
 			pwPassword := lines[i+2]
-			letterOccurs := strings.Count(pwPassword, pwLetter)
 			
-			if letterOccurs >= int(pwMin) && letterOccurs <= int(pwMax){
-				numOfValidPasswords+=1
-			}			
+			if string(pwPassword[pwMin-1]) == pwLetter{
+				controlSum+=1
+			}
+			
+			if string(pwPassword[pwMax-1]) == pwLetter{
+				controlSum+=1
+			}
+			
+			if controlSum == 1{
+			numOfValidPasswords+=1
+			}
 		}
 	}
 	
